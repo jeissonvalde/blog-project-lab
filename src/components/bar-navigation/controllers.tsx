351e83ad0ef1c32923f950df6f56dd3e6cb07c3f
Jeisson A Valderrama <jeissonvalderramam@gmail.com>
2023-01-03 23:37:18 -0500

    Prepare navigation method.

--- src/components/bar-navigation/controllers.tsx
+++ src/components/bar-navigation/controllers.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+declare global {
+  interface Window {
+    currentArticle: any
+  }
+}
 
 function clickLiNavigation (route: { Container: any }, e: any) {
-  let rootElem = document.getElementById('root') as HTMLElement
+  let rootElem = document.getElementById('root') as HTMLElement,
+    AppElem = document.querySelector('.App') as  HTMLElement,
+    childToRemove = AppElem.firstChild as  HTMLElement
 
-
-  return ReactDOM.hydrate(<route.Container<HTMLElement> />, rootElem)
+  
 }
 
 export default {
